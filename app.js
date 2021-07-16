579c15a95622bc6a166006c5925640e287d44817
Luis Fernando <desarrollo@fcomunicaciones.com>
2021-07-16 16:12:04 -0500

    Tipos de contenido text(plain y application/json para el WriteHead

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 
     const http = require('http');
 
-    http.createServer( (request, response) => {
+    http.createServer( (request, response) => { 
+        
+        // request => Lo que se solicita desde cualquier cliente (mobiles, celular, pc)
+        // response => Lo que retornará mi servidor al cliente
 
-        response.write( 'Hola munidollo' );
-        response.end();
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+
+        const persona = {
+            id: 1,
+            nombre: 'Fernando'
+        };
+
+        response.write( JSON.stringify(persona));
+        response.end(); // Con .end() decimos a Node que ya terminamos nuestros procesos y no debe haber más flujo
     })
     .listen( 8080 );
-
-    console.log('Escuchando desde:', 8080);
