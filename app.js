8b343ada217538ff428a1897be95f90d64142fcd
Luis Fernando <desarrollo@fcomunicaciones.com>
2021-07-16 16:20:51 -0500

    Explorando contenido csv y propiedades del header

--- app.js
+++ app.js
@@ -6,14 +6,14 @@
         // request => Lo que se solicita desde cualquier cliente (mobiles, celular, pc)
         // response => Lo que retornará mi servidor al cliente
 
-        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.setHeader('Content-Disposition', 'attachment; filename=test_file.csv')
+        response.writeHead(200, { 'Content-Type': 'application/csv' });
 
-        const persona = {
-            id: 1,
-            nombre: 'Fernando'
-        };
-
-        response.write( JSON.stringify(persona));
+        response.write('id, nombre\n');
+        response.write('1, Fernando\n');
+        response.write('2, Juan\n');
+        response.write('3, Pedro\n');
+        response.write('4, Manuel\n');
         response.end(); // Con .end() decimos a Node que ya terminamos nuestros procesos y no debe haber más flujo
     })
     .listen( 8080 );
